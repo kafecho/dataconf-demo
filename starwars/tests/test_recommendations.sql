54a161722846f3048e6ebebc4d755580283d682f
Guillaume Belrose <guillaumebelrose@johnlewis.co.uk>
2023-10-16 21:13:02 +0200

    add test file

---
+++ starwars/tests/test_recommendations.sql
@@ -0,0 +1,19 @@
+{{ config(tags=['unit-test']) }}
+
+  {% call dbt_unit_testing.test('recommendations', 'should recommend planets based on preferences') %}
+
+    {% call dbt_unit_testing.mock_ref('planets') %}
+        name | terrain
+        'Mustafar' | 'volcanoes, lava rivers, mountains, caves'
+    {% endcall %}
+
+    {#% call dbt_unit_testing.mock_ref('preferences') %}
+        customer_name | vacation_type
+        'Darth Vader' | 'volcanoes'
+    {% endcall %#}
+
+    {% call dbt_unit_testing.expect() %}
+        customer_name | recommended_planet
+        'Darth Vader' | 'Mustafar'
+    {% endcall %}
+  {% endcall %}
