9bcada616ea0cd2c47cbfc61d9cd794693febd7b
Guillaume Belrose <guillaumebelrose@johnlewis.co.uk>
2023-10-16 22:42:28 +0200

    fail for Luke

--- starwars/models/recommendations.sql
+++ starwars/models/recommendations.sql
@@ -1,11 +1,3 @@
--- SELECT
---     'Darth Vader' AS customer_name, 
---     'Mustafar' AS recommended_planet
-
 SELECT
-    preferences.customer_name,
-    planets.name as recommended_planet 
-FROM {{ ref('preferences')}}
-JOIN {{ ref('planets') }}
--- ON planets.terrain like '%' || preferences.vacation_type || '%' 
-ON string_to_array(preferences.vacation_type, ', ') <@ (string_to_array(planets.terrain, ', '))
+    'Darth Vader' AS customer_name, 
+    'Mustafar' AS recommended_planet
