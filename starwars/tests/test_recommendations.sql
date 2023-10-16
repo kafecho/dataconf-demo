bb58bd7a88c435726df972a2c541b08c94e1d093
Guillaume Belrose <guillaumebelrose@johnlewis.co.uk>
2023-10-16 22:05:27 +0200

    end state of recommendation

--- starwars/tests/test_recommendations.sql
+++ starwars/tests/test_recommendations.sql
@@ -5,15 +5,21 @@
     {% call dbt_unit_testing.mock_ref('planets') %}
         name | terrain
         'Mustafar' | 'volcanoes, lava rivers, mountains, caves'
+        'Alderaan' | 'grasslands, mountains'
     {% endcall %}
 
-    {#% call dbt_unit_testing.mock_ref('preferences') %}
+    {% call dbt_unit_testing.mock_ref('preferences') %}
         customer_name | vacation_type
-        'Darth Vader' | 'volcanoes'
-    {% endcall %#}
+        'Darth' | 'caves, volcanoes'
+        'Leia'  | 'grasslands'
+        'Luke'  | 'desert'
+    {% endcall %}
 
     {% call dbt_unit_testing.expect() %}
         customer_name | recommended_planet
-        'Darth Vader' | 'Mustafar'
+        'Darth' | 'Mustafar'
+        'Leia'  | 'Alderaan'
+        'Luke'  | null
     {% endcall %}
   {% endcall %}
+
