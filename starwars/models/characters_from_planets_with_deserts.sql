0b9329a4a6fd55b13a6c77cc1c2fb493901227ba
Guillaume Belrose <guillaumebelrose@johnlewis.co.uk>
2023-10-17 10:42:45 +0200

    Initial state for data pipeline demo

--- starwars/models/characters_from_planets_with_deserts.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM {{ ref('characters_and_planets')}}
-WHERE terrain LIKE '%desert%'
-ORDER BY homeworld DESC
-
--- starwars/models/characters_from_planets_with_mountains.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM {{ ref('characters_and_planets')}}
-WHERE terrain LIKE '%mountains%'
-ORDER BY homeworld DESC
-
